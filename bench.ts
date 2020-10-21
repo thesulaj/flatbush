7e1a12cf0cfb1b8c728f1fef44c41e4e4277d14a
Saimir Sulaj <saimir@urbanlogiq.com>
2020-10-21 11:49:37 -0700

    typescriptified

---
+++ bench.ts
@@ -0,0 +1,129 @@
+/* eslint-disable no-console */
+// @ts-ignore
+import RBush from 'rbush'; // eslint-disable-line
+// @ts-ignore
+import rbushKNN from 'rbush-knn';  // eslint-disable-line
+import Flatbush from './src/index';
+
+const N = 1000000;
+const K = 1000;
+const nodeSize = 16;
+
+console.log(`${N} rectangles`);
+console.log(`node size: ${nodeSize}`);
+console.log('');
+
+function addRandomBox(arr: number[], boxSize: number) {
+  const x = Math.random() * (100 - boxSize);
+  const y = Math.random() * (100 - boxSize);
+  const x2 = x + Math.random() * boxSize;
+  const y2 = y + Math.random() * boxSize;
+  arr.push(x, y, x2, y2);
+}
+
+const coords: number[] = [];
+for (let i = 0; i < N; i++) addRandomBox(coords, 1);
+
+const boxes100: number[] = [];
+const boxes10: number[] = [];
+const boxes1: number[] = [];
+for (let i = 0; i < K; i++) {
+  addRandomBox(boxes100, 100 * Math.sqrt(0.1));
+  addRandomBox(boxes10, 10);
+  addRandomBox(boxes1, 1);
+}
+
+console.time('flatbush');
+const index = new Flatbush(N, nodeSize);
+for (let i = 0; i < coords.length; i += 4) {
+  index.add(
+    coords[i],
+    coords[i + 1],
+    coords[i + 2],
+    coords[i + 3],
+  );
+}
+index.finish();
+console.timeEnd('flatbush');
+
+console.log(`index size: ${index.data.byteLength.toLocaleString()}`);
+
+function benchSearch(boxes: number[], name: string, warmup?: boolean) {
+  const id = `${K} searches ${name}`;
+  if (!warmup) console.time(id);
+  for (let i = 0; i < boxes.length; i += 4) {
+    index.search(boxes[i], boxes[i + 1], boxes[i + 2], boxes[i + 3]);
+  }
+  if (!warmup) console.timeEnd(id);
+}
+
+function benchNeighbors(k: number, M: number, warmup?: boolean) {
+  const id = `${k} searches of ${M} neighbors`;
+  if (!warmup) console.time(id);
+  for (let i = 0; i < k; i++) {
+    index.neighbors(coords[4 * i], coords[4 * i + 1], M);
+  }
+  if (!warmup) console.timeEnd(id);
+}
+
+benchSearch(boxes1, '0.01%', true);
+benchSearch(boxes100, '10%');
+benchSearch(boxes10, '1%');
+benchSearch(boxes1, '0.01%');
+
+benchNeighbors(K, 1, true);
+benchNeighbors(K, 100);
+benchNeighbors(1, N);
+benchNeighbors(N / 10, 1);
+
+const dataForRbush = [];
+for (let i = 0; i < coords.length; i += 4) {
+  dataForRbush.push({
+    minX: coords[i],
+    minY: coords[i + 1],
+    maxX: coords[i + 2],
+    maxY: coords[i + 3],
+  });
+}
+
+console.log('');
+console.time('rbush');
+const rbushIndex = new RBush(nodeSize).load(dataForRbush);
+console.timeEnd('rbush');
+
+function benchSearchRBush(boxes: number[], name: string, warmup?: boolean) {
+  const boxes2 = [];
+  for (let i = 0; i < boxes.length; i += 4) {
+    boxes2.push({
+      minX: boxes[i],
+      minY: boxes[i + 1],
+      maxX: boxes[i + 2],
+      maxY: boxes[i + 3],
+    });
+  }
+  const id = `${K} searches ${name}`;
+  if (!warmup) console.time(id);
+  for (let i = 0; i < boxes2.length; i++) {
+    rbushIndex.search(boxes2[i]);
+  }
+  if (!warmup) console.timeEnd(id);
+}
+
+function benchNeighborsRBush(k: number, M: number, warmup?: boolean) {
+  const id = `${k} searches of ${M} neighbors`;
+  if (!warmup) console.time(id);
+  for (let i = 0; i < k; i++) {
+    rbushKNN(rbushIndex, coords[4 * i], coords[4 * i + 1], M);
+  }
+  if (!warmup) console.timeEnd(id);
+}
+
+benchSearchRBush(boxes1, '0.01%', true);
+benchSearchRBush(boxes100, '10%');
+benchSearchRBush(boxes10, '1%');
+benchSearchRBush(boxes1, '0.01%');
+
+benchNeighborsRBush(K, 1, true);
+benchNeighborsRBush(K, 100);
+benchNeighborsRBush(1, N);
+benchNeighborsRBush(N / 10, 1);
--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,20 @@
 import resolve from '@rollup/plugin-node-resolve';
 import buble from '@rollup/plugin-buble';
 import {terser} from 'rollup-plugin-terser'
+import ts from '@wessberg/rollup-plugin-ts';
 
 const output = (file, plugins) => ({
-    input: 'index.js',
-    output: {
-        name: 'Flatbush',
-        format: 'umd',
-        indent: false,
-        file
-    },
-    plugins
+  input: './src/index.ts',
+  output: {
+    name: 'Flatbush',
+    format: 'umd',
+    indent: false,
+    file
+  },
+  plugins
 });
 
 export default [
-    output('flatbush.js', [resolve(), buble()]),
-    output('flatbush.min.js', [resolve(), terser({compress: {passes: 2}}), buble()])
+  output('./build/flatbush.js', [ts(), resolve(), buble()]),
+  output('./build/flatbush.min.js', [ts(), resolve(), terser({compress: {passes: 2}}), buble()])
 ];
